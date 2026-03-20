be3ddd66e4d736ae840529536923746e5bdd87ed
Yuya Tanaka <yuya.tanaka0912@gmail.com>
2026-03-20 16:37:53 +0000

    feat: 動画生成機能を追加

---
+++ video_generation/__init__.py
@@ -0,0 +1,5 @@
+"""Video generation module using LTX-2 via diffusers."""
+
+from .generate import VideoGenerationConfig, generate_video, main
+
+__all__ = ["VideoGenerationConfig", "generate_video", "main"]
